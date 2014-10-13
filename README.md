e179a8ca1ce6105e9d369470005535a92980441c
Jason (Jayce^) Hall <jayce@lug-nut.com>
2014-10-12 20:18:47 -0600

    A little bit more documentation

--- README.md
+++ README.md
@@ -9,8 +9,32 @@ version 0.001
 # SYNOPSIS
 
 use WebService::FamilySearch;
+
 my $FS = WebService::FamilySearch->new;
+
 $FS->oauth\_token($oauth\_token);
+
+# DOCUMENTATION
+
+Developer Documentation available at: 
+
+https://familysearch.org/developers/docs/api
+
+Getting developer credentials can be done at:
+
+https://familysearchcommunity.force.com/Developer/
+
+# ENVIRONMENTS
+
+## SANDBOX
+
+The sandbox is an empty environment that you can use to play around with any data. Applications can be setup thought the developer site, as well as user creation and credentials
+
+## BETA
+
+The BETA environment is a clone of the live tree, and you will use your own OAuth credentials to log in and work with the real data. Credentials must be approved from the FamilySearch group.
+
+
 
 # AUTHOR
 
@@ -22,3 +46,11 @@ This software is copyright (c) 2014 by Jason (Jayce^) Hall.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
+
+# POD ERRORS
+
+Hey! **The above document had some coding errors, which are explained below:**
+
+- Around line 43:
+
+    You forgot a '=back' before '=head1'
--- lib/WebService/FamilySearch.pm
+++ lib/WebService/FamilySearch.pm
@@ -4,7 +4,9 @@ package WebService::FamilySearch;
 =head1 SYNOPSIS
 
 use WebService::FamilySearch;
+
 my $FS = WebService::FamilySearch->new;
+
 $FS->oauth_token($oauth_token);
 
 =cut
@@ -49,5 +51,37 @@ has host =>  (
   },
 );
 
+sub current_person {
+  my $self = shift;
+  my $ret = $self->_http->get( $self->host . '/platform/tree/current-person', { headers => $self->_default_http_args });
+  debug decode_json($ret->{'content'});
+  my $person = WebService::FamilySearch::Person->new( decode_json( $ret->{'content'} ) );
+};
 
+
+1;
 __END__
+
+=head1 DOCUMENTATION
+
+Developer Documentation available at: 
+
+https://familysearch.org/developers/docs/api
+
+Getting developer credentials can be done at:
+
+https://familysearchcommunity.force.com/Developer/
+
+=head1 ENVIRONMENTS
+
+=head2 SANDBOX
+
+The sandbox is an empty environment that you can use to play around with any data. Applications can be setup thought the developer site, as well as user creation and credentials
+
+=head2 BETA
+
+The BETA environment is a clone of the live tree, and you will use your own OAuth credentials to log in and work with the real data. Credentials must be approved from the FamilySearch group.
+
+=over
+
+
